96f76df8529df07a6a4f61b80c80716b890ba7c9
Name <98811739+NicoloMagosso@users.noreply.github.com>
2023-05-10 23:47:48 +0200

    Versione 3
    Test Unitario creato, non disponendo delle estensioni necessarie non riesco a far avviare il processo.

---
+++ AddressGenerator/AddressGenerator/UnitTestcs.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AddressGenerator
+{
+    [TestClass]
+    public class AddressGeneratorTests
+    {
+        [TestMethod]
+        public void TestGenerateIPv4()
+        {
+            uint address = 0xc0a80101; // 192.168.1.1
+            AddressGenerator_Class addressGenerator = new AddressGenerator_Class(address);
+            string ipv4 = addressGenerator.generateIPv4();
+            Assert.AreEqual("192.168.1.1", ipv4);
+        }
+
+        [TestMethod]
+        public void TestGenerateSubnet()
+        {
+            uint address = 0xc0a80101;
+            AddressGenerator_Class addressGenerator = new AddressGenerator_Class(address);
+            string subnet = addressGenerator.generateSubnet(24);
+            Assert.AreEqual("255.255.255.0", subnet);
+        }
+    }
+
+}
